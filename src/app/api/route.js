979ec6d66be101f188956dab762244eca3c5f295
fecarneiro <fe.iacarneiro@gmail.com>
2025-08-27 16:06:30 -0300

    Refactor extraction function to utilize file input and improve response handling

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,28 +1,11 @@
 import dotenv from 'dotenv';
+import fs from 'fs';
 import OpenAI from 'openai';
 
 dotenv.config();
+const client = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
-const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
-
-export async function extraction(prompt) {
-  try {
-    const completion = await openai.chat.completions.create({
-      model: 'gpt-4o-mini',
-      messages: [{ role: 'user', content: prompt }],
-      response_format: { type: 'json_object' },
-    });
-
-    const response = completion.choices[0]?.message?.content;
-    console.log(response);
-    if (!response) throw new Error('Resposta vazia da OpenAI');
-    return JSON.parse(response);
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-const prompt = `
+const instructions = `
 Extraia deste documento: data da emissão, o nome e cnpj do tomador, o nome da mercadoria, o valor total da mercadoria, a origem (cidade e estado) e destino (cidade e estado).
 
 Sua resposta obrigatoriamente deverá ser um JSON neste formato:
@@ -46,4 +29,39 @@ destination: {
 },
 `;
 
-console.log(extraction(prompt));
+export async function extraction() {
+  try {
+    const file = await client.files.create({
+      file: fs.createReadStream('kona.pdf'),
+      purpose: 'user_data',
+    });
+
+    const response = await client.responses.create({
+      model: 'gpt-4o-mini',
+      input: [
+        {
+          role: 'user',
+          content: [
+            {
+              type: 'input_file',
+              file_id: file.id,
+            },
+            {
+              type: 'input_text',
+              text: 'Quais as principais informações do PDF?',
+            },
+          ],
+        },
+      ],
+    });
+
+    console.log(response.output_text);
+
+    if (!extractedData) throw new Error('Resposta vazia da OpenAI');
+    return JSON.parse(extractedData);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+console.log(extraction());
