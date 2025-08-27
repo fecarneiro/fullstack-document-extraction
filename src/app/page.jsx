51ff75e40faabdd0e70d9b30e1ac91b96a0185f3
fecarneiro <fe.iacarneiro@gmail.com>
2025-08-27 13:58:07 -0300

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,29 @@
+export default function Page() {
+  return (
+    <div className="p-10 flex flex-col items-center rounded-lg shadow-xl gap-4 max-w-md mx-auto">
+      <h1 className="text-2xl font-bold">Document Extractor</h1>
+      <p>
+        Selecione um documento PDF para fazer upload e extrair as informações em
+        JSON
+      </p>
+      <button class="px-4 py-2 font-bold text-white bg-blue-500 rounded">
+        Upload
+      </button>
+    </div>
+  );
+}
+
+// <div class="p-4 rounded shadow max-w-md">
+
+//   <h3 class="font-bold text-xl mb-2">
+//     <!-- card title -->
+//   </h3>
+
+//   <!-- card content -->
+
+//   <button class="bg-blue-500 text-white p-2 block mt-4">
+//     <!-- call to action -->
+//   </button>
+
+// </div>
+<div className="max-w-2xl mx-auto flex flex-col gap-6 p-6 border rounded-lg shadow"></div>;
