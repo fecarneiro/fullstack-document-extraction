67fe4279b422a4dee5eff29060cc0602e2892fe6
fecarneiro <fe.iacarneiro@gmail.com>
2025-08-27 17:19:03 -0300

    Update dependencies and improve extraction function

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,42 +1,60 @@
 import dotenv from 'dotenv';
 import fs from 'fs';
 import OpenAI from 'openai';
+import { zodTextFormat } from 'openai/helpers/zod';
+import { z } from 'zod';
+
+// const ExtractedData = z.object({
+//   transport_date: z.string(),
+//   shipper: { cnpj: z.string(), name: z.string() },
+//   goods: { name: z.string(), value: z.number() },
+//   origin: { city: z.string(), uf: z.string() },
+//   destination: { city: z.string(), uf: z.string() },
+// });
 
 dotenv.config();
 const client = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 const instructions = `
-Extraia deste documento: data da emissão, o nome e cnpj do tomador, o nome da mercadoria, o valor total da mercadoria, a origem (cidade e estado) e destino (cidade e estado).
+Extraia deste documento: 
+- data da emissão
+- o nome do tomador
+- o cnpj do tomador
+- o nome da mercadoria
+- o valor total da mercadoria
+- a origem (cidade e estado)
+- destino (cidade e estado).
 
 Sua resposta obrigatoriamente deverá ser um JSON neste formato:
-
-transport_date,
-shipper: {
-  cnpj,
-  name,
-},
-goods: {
-  name,
-  value_brl,
-},
-origin: {
-  city,
-  uf,
-},
-destination: {
-  city,
-  uf,
-},
+{
+  "transport_date",
+  "shipper": {
+    "cnpj",
+    "name",
+  },
+  goods: {
+    "name",
+    "value_brl",
+  },
+  origin: {
+    "city",
+    "uf",
+  },
+  destination: {
+    "city",
+    "uf",
+  },
+}
 `;
 
-export async function extraction() {
+export async function extractData(instructions) {
   try {
     const file = await client.files.create({
-      file: fs.createReadStream('kona.pdf'),
+      file: fs.createReadStream('test.pdf'),
       purpose: 'user_data',
     });
 
-    const response = await client.responses.create({
+    const response = await client.responses.parse({
       model: 'gpt-4o-mini',
       input: [
         {
@@ -48,20 +66,24 @@ export async function extraction() {
             },
             {
               type: 'input_text',
-              text: 'Quais as principais informações do PDF?',
+              text: instructions,
             },
           ],
         },
       ],
+      text: { format: { type: 'json_object' } },
+      // text: {
+      //   format: zodTextFormat(ExtractedData, 'extractedData'),
+      // },
     });
 
     console.log(response.output_text);
 
-    if (!extractedData) throw new Error('Resposta vazia da OpenAI');
-    return JSON.parse(extractedData);
+    if (!extractData) throw new Error('Resposta vazia da OpenAI');
+    return JSON.parse(extractData);
   } catch (error) {
     console.error(error);
   }
 }
 
-console.log(extraction());
+console.log(extractData(instructions));
